b187b12dc2283335da80af45ce11c3bd51fbe59e
dhaheenrahman <dhaheen.h@gmail.com>
2024-11-26 14:07:40 +0530

    day 15 updated

---
+++ p1/day14/modules/main.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+#include "a.h"
+#include "b.h"
+using namespace std;
+
+int main(){
+    a();
+    b();
+    return 0;
+}
---
+++ p1/day15/railway_booking_modules/Menu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+#include "Menu.h"
+
+using std::cout;
+
+void printMenu() {
+	cout << "Menu:\n";
+	cout << "1. Create Booking\n";
+	cout << "2. Display All Bookings\n";
+	cout << "3. Edit Booking\n";
+	cout << "4. Delete Booking\n";
+	cout << "5. Exit\n";
+}
